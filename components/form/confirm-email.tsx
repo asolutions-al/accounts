cfc21a618538d9b64a03d76be961870686f55ce4
anxhi <anxhirr@gmail.com>
2025-08-02 22:29:53 +0200

    fix: autofocus on confirm input

--- components/form/confirm-email.tsx
+++ components/form/confirm-email.tsx
@@ -105,6 +105,7 @@ export function ConfirmEmailForm({
                           {...field}
                           maxLength={6}
                           className="text-center text-lg tracking-widest"
+                          autoFocus
                         />
                       </FormControl>
                       <FormMessage />
